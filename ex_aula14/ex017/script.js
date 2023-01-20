b022b893af061c5944dffa6e7eeb15301f381be8
Eliasgsn <gabrielsilva20012005@hotmail.com>
2023-01-20 15:49:22 -0200

    ex 17

--- ex_aula14/ex017/script.js
+++ ex_aula14/ex017/script.js
@@ -1,1 +1,12 @@
-alert("Olá!")
+function contar () {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if (ini.value == 0 || fim.value == 0 || passo.value == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        alert('TUDO OK!')
+    }
+
+}
